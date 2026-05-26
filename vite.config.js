b19b28ba0bfa50f1f62de788dabc3a0132ee0138
posyandumahap-rgb <posyandumahap@gmail.com>
2026-05-26 08:37:29 +0700

    Add base44 plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
+import base44 from '@base44/vite-plug-in'
 // https://vite.dev/config/
 export default defineConfig({
   logLevel: 'error', // Suppress warnings, only show errors
